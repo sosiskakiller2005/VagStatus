c5794f32f8780c3d738d0755bbf23321969473ca
Влад Козлов <vkozlov15012005@gmail.com>
2024-11-24 20:58:01 +0300

    Добавьте файлы проекта.

---
+++ VagStatus/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using VagStatus.Model;
+
+namespace VagStatus
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static VagStatusDbEntities _context;
+        public static VagStatusDbEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new VagStatusDbEntities();
+            }
+            return _context;
+        }
+    }
+}
