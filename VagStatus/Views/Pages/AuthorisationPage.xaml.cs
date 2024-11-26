98c58ed4c96943e91e2a976ef3cc5661906edce3
Влад Козлов <vkozlov15012005@gmail.com>
2024-11-26 11:12:09 +0300

    Авторизация

---
+++ VagStatus/Views/Pages/AuthorisationPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VagStatus.AppData;
+
+namespace VagStatus.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorisationPage.xaml
+    /// </summary>
+    public partial class AuthorisationPage : Page
+    {
+        public AuthorisationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (IdTb.Text != string.Empty && PasswordTb.Password != string.Empty)
+            {
+                if (AuthorisationHelper.Authorise(IdTb.Text, PasswordTb.Password))
+                {
+                    FrameHelper.selectedFrame.Navigate(new MenuPage());
+                }
+            }
+            else
+            {
+                MessageBoxHelper.Error("Заполните все поля для ввода.");
+            }
+        }
+    }
+}
--- VagStatus/Views/Pages/MenuPage.xaml.cs
+++ VagStatus/Views/Pages/MenuPage.xaml.cs
@@ -10,22 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
-using VagStatus.AppData;
-using VagStatus.Views.Pages;
 
-namespace VagStatus.Views.Windows
+namespace VagStatus.Views.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MenuPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MenuPage : Page
     {
-        public MainWindow()
+        public MenuPage()
         {
             InitializeComponent();
-            FrameHelper.selectedFrame = MainFrm;
-            MainFrm.Navigate(new AuthorisationPage());
         }
     }
 }
